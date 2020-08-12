5c2571f21969f944a208185b3c98e930509220d3
zwe-htet-paing <zwehetpaing123@gmail.com>
2020-08-12 11:16:49 +0630

    first commit

---
+++ MyMVCApplication/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        /*
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        */
+        public string Index()
+        {
+            return "This is Index action method of StudentController";
+        }
+    }
+}
